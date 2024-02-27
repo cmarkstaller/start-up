8199a2c5211d4ad837ad21055514e7aab3523b5d
cmarkstaller <cmarkstaller@gmail.com>
2024-02-27 11:57:03 -0700

    going back to login js to fix storage additions

--- person.js
+++ person.js
@@ -11,9 +11,46 @@ function login() {
     dictionary.set(nameEl, new Person(nameEl));
     
     localStorage.setItem("dictionary", dictionary);
+    localStorage.setItem("username", nameEl);
     
+    console.log("I made it here");
+
     window.location.href = "main.html";
   }
+
+function populatePerson() {
+    dictionary = JSON.parse(localStorage.getItem('dictionary'));
+    console.log(localStorage.getItem("username"));
+    
+    // let userObject = dictionary.get(localStorage.getItem("username"));
+
+    // const userTag = document.createElement('h1');
+    // userTag.textContent = localStorage.getItem("username");
+
+    // const cardEl = document.querySelector('#personal card');
+    // cardEl.appendChild(userTag);
+
+
+
+    // <div class="card personal" id="personal card">
+    //         <h1>Username</h1>
+    //         <div class="goals">
+    //             <label>
+    //                 <input type="checkbox">
+    //                 <span></span>
+    //                 <p>Goal 1</p>
+    //             </label>
+    //             <label>
+    //                 <input type="checkbox">
+    //                 <span></span>
+    //                 <p>Goal 2</p>
+    //             </label>
+    //             <label>
+    //                 <button class="btn"><i class='bx bx-plus-circle'></i></button>
+    //             </label>
+    //         </div>
+    //     </div>
+}  
 
 class Person {
     userName;
