c2715eda0f67584070e7cbe74f3141fd9192b12d
cmarkstaller <cmarkstaller@gmail.com>
2024-03-22 18:23:16 -0600

    Authentication functionality added to index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const express = require('express');
+const app = express();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
+
 class Person {
   userName;
   goals;
@@ -24,32 +32,94 @@ class Person {
 
 var dictionary = new Map();
 
-const express = require('express');
-const app = express();
-const DB = require('./database.js');
-
 // The service port. In production the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
 // Serve up the frontend static content hosting
 app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
 // Router for service endpoints
 const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.username)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.username, req.body.password);
+
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.username);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:username', async (req, res) => {
+  const user = await DB.getUser(req.params.username);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.username, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
 // AddUser
-apiRouter.post('/addUser', (req, res) => {
+secureApiRouter.post('/addUser', (req, res) => {
   dictionary.set(req.body.userName, new Person(req.body.userName, req.body.goals, req.body.friends));
   DB.createPerson(req.body.userName, req.body.goals, req.body.friends);
   res.status(200).send('Resource updated successfully');
 });
 
-// // GetUser
-apiRouter.get('/getUser/:username', (req, res) => {
+// GetUser
+secureApiRouter.get('/getUser/:username', (req, res) => {
   const username = req.params.username;
   const user = DB.getPerson(username);
   
@@ -71,7 +141,7 @@ apiRouter.get('/getUser/:username', (req, res) => {
 
 
 // ListUsers
-apiRouter.get('/listUsers', async (req, res) => {
+secureApiRouter.get('/listUsers', async (req, res) => {
   //const valuesArray = Array.from(dictionary.values());
   const valuesArray = await DB.listUsers();
 
@@ -86,7 +156,7 @@ apiRouter.get('/listUsers', async (req, res) => {
 });
 
 // ListUsernames
-apiRouter.get('/listUsernames', async (req, res) => {
+secureApiRouter.get('/listUsernames', async (req, res) => {
   //const valuesArray = Array.from(dictionary.keys());
   const valuesArray = await DB.listUsernames();
 
@@ -99,17 +169,31 @@ apiRouter.get('/listUsernames', async (req, res) => {
 })
 
 // UpdateUser
-apiRouter.put('/updateUser', (req, res) => {
+secureApiRouter.put('/updateUser', (req, res) => {
   //dictionary.set(req.body.userName, new Person(req.body.userName, req.body.goals, req.body.friends));
   //console.log(dictionary);
   DB.updateUser(req.body.username, req.body.goals, req.body.friends);
   res.status(200).send('Resource updated successfully');
 });
 
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
