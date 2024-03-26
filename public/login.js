5d97e7d6dcf32c09d15f86b134d04c5e47066c12
cmarkstaller <cmarkstaller@gmail.com>
2024-03-26 17:58:14 -0600

    Set up login.js on client side to do websocket

--- public/login.js
+++ public/login.js
@@ -208,6 +208,8 @@ async function login() {
   if (!dictionary.has(usernameEl)) {
       await addUser(usernameEl);
   }
+  
+  configureWebSocket();
 
   window.location.href = "main.html";
 }
@@ -404,4 +406,39 @@ function displayQuote(data) {
       containerEl.appendChild(quoteEl);
       containerEl.appendChild(authorEl);
   });
+}
+
+function configureWebSocket() {
+  const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+  this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+  this.socket.onopen = (event) => {
+    this.displayMsg('system', 'game', 'connected');
+  };
+  this.socket.onclose = (event) => {
+    this.displayMsg('system', 'game', 'disconnected');
+  };
+  this.socket.onmessage = async (event) => {
+    const msg = JSON.parse(await event.data.text());
+    if (msg.type === GameEndEvent) {
+      this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+    } 
+    else if (msg.type === GameStartEvent) {
+      this.displayMsg('player', msg.from, `started a new game`);
+    }
+  };
+}
+
+function displayMsg(cls, from, msg) {
+  const chatText = document.querySelector('#player-messages');
+  chatText.innerHTML =
+    `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+}
+
+function broadcastEvent(from, type, value) {
+  const event = {
+    from: from,
+    type: type,
+    value: value,
+  };
+  this.socket.send(JSON.stringify(event));
 }
