9d13334dfb345868417d7c9875b6180edfdc90ac
cmarkstaller <cmarkstaller@gmail.com>
2024-04-11 14:10:09 -0600

    Router is implimented

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login';
+import { Main } from './main';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/login' element={<Login />} />
+        <Route path='/main' element={<Main />} />
+        <Route path='*' element={<NotFound />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+  }
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Main() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>Main displayed here</div>
+    </main>
+  );
+}
